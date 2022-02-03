caf60aa8c9a01b7978ec4565e4801a2659320f2f
Der Alex <alexander.thimm@googlemail.com>
2022-02-03 10:39:21 +0100

    sample for images not working when using ~/assets or ~assets as a path

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- Not working when running npm 'run dev' -->
+    <picture>
+      <source srcset="~/assets/img/test-img.jpg" media="(max-width: 2000px)">
+      <img src="~/assets/img/test-img.jpg" alt="test image">
+    </picture>
+
+    <!-- Not working when running npm 'run dev' -->
+    <picture>
+      <source srcset="~assets/img/test-img.jpg" media="(max-width: 2000px)">
+      <img src="~assets/img/test-img.jpg" alt="test image">
+    </picture>
+
+    <!-- Works with 'npm run dev' and 'npm run start' -->
+    <picture>
+      <source srcset="assets/img/test-img.jpg" media="(max-width: 2000px)">
+      <img src="assets/img/test-img.jpg" alt="test image">
+    </picture>
+
+    <!-- Works with 'npm run dev' and 'npm run start' -->
+    <picture>
+      <source srcset="/assets/img/test-img.jpg" media="(max-width: 2000px)">
+      <img src="/assets/img/test-img.jpg" alt="test image">
+    </picture>
+  </div>
+</template>
